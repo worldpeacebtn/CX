882632891a6b962660d30b931d2a33c65e164752
Save the World now <worldpeacebtn@gmail.com>
2025-11-19 19:41:54 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./src/style.css"; // <-- make sure your CSS file is imported here
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
