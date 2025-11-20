114c8208b588ff0a25b7dd78c2c2122e581dd86b
Save the World now <worldpeacebtn@gmail.com>
2025-11-20 02:34:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ export default function App() {
           <Link to="/assets">Assets</Link>
           <Link to="/contact">Contact</Link>
         </nav>
+        <div className="hudBar"></div>
       </header>
 
       <main>
