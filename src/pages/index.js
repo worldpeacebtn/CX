13ad47bc7e9d547f55ebc1af8d0c6d7715a7a206
Save the World now <worldpeacebtn@gmail.com>
2025-11-21 19:12:25 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,4 @@ export { default as AssetsPage } from "./AssetsPage.jsx";
 export { default as ContactPage } from "./ContactPage.jsx";
 export { default as Collaboration } from "./Collaboration.jsx";
 export { default as Evidence } from "./Evidence.jsx";
+export { default as Home } from "./Home.jsx"; // -> if you give your Home page this filename
