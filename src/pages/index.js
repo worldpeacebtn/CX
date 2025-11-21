db99bb684bb33ee448f975595eb50e4796c71511
Save the World now <worldpeacebtn@gmail.com>
2025-11-21 14:23:48 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import HeroCanvas from "../components/HeroCanvas";
-
-export default function Home() {
-  return <HeroCanvas />;
-}
+// src/pages/index.jsx
+export { default as Home } from './Home.jsx';
+export { default as SlidesPage } from './SlidesPage.jsx';
+export { default as TimelinePage } from './TimelinePage.jsx';
+export { default as AssetsPage } from './AssetsPage.jsx';
+export { default as ContactPage } from './ContactPage.jsx';
