d5d6fb3b6f2e50edb5bf451f8d85c48f9109930f
Save the World now <worldpeacebtn@gmail.com>
2025-11-21 19:03:35 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-// src/pages/index.jsx
-export { default as Home } from './Home.jsx';
-export { default as SlidesPage } from './SlidesPage.jsx';
-export { default as TimelinePage } from './TimelinePage.jsx';
-export { default as AssetsPage } from './AssetsPage.jsx';
-export { default as ContactPage } from './ContactPage.jsx';
+// src/pages/index.js
+export { default as SlidesPage } from "./SlidesPage.jsx";
+export { default as TimelinePage } from "./TimelinePage.jsx";
+export { default as AssetsPage } from "./AssetsPage.jsx";
+export { default as ContactPage } from "./ContactPage.jsx";
+export { default as Collaboration } from "./Collaboration.jsx";
+export { default as Evidence } from "./Evidence.jsx";
