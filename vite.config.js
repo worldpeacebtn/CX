3ac01098630f318c3807c73e8792a965e8d176c5
Save the World now <worldpeacebtn@gmail.com>
2025-11-22 03:30:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-  exclude: ["three"]
-},
-build: {
-  rollupOptions: {
-    external: ["three"],
+  build: {
+    // Increase warning limit so your R3F chunk doesn't scare you
+    chunkSizeWarningLimit: 1500,
+
+    rollupOptions: {
+      output: {
+        // This allows smart splitting
+        manualChunks(id) {
+          if (id.includes("three") || id.includes("@react-three")) {
+            return "three-vendors";
+          }
+        }
+      }
+    },
     target: "es2020"
   }
 });
