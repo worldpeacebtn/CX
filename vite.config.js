cae7a2b0388d8fe8d51d7cd0c3630c9c904c985e
Save the World now <worldpeacebtn@gmail.com>
2025-11-22 03:27:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+  exclude: ["three"]
+},
+build: {
+  rollupOptions: {
+    external: ["three"],
   build: {
     target: "es2020"
   }
